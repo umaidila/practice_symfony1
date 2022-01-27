46cd2e67b8bd7eec6f53a43d712ebdb2030b9bf8
[umaidila] <[frenirus125@gmail.com]>
2022-01-27 17:57:24 +1000

    dev deploy

--- Scripts/deploy_dev.sh
+++ Scripts/deploy_dev.sh
@@ -1,4 +1,4 @@
 git checkout -b dev
 git commit -a -m "dev deploy"
 git push origin dev
-ssh root@91.203.192.213 'cd /var/www/html/project_dev/Scripts/build.sh'
+ssh root@91.203.192.213 'bash /var/www/html/project_dev/Scripts/build.sh'
